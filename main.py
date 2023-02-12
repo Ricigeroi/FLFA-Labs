0525d5d0fceb1255a7251c2adaadd01b460ce5aa
Andrei Bardier <andrey.bardier@gmail.com>
2023-02-12 22:26:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
         "D": ["cD", "dR", "d"],
         "R": ["bR", "f"]
     }
-
     S = "S"
     grammar = Grammar(Vn, Vt, P, S)
     finite_automaton = grammar.to_finite_automaton()
@@ -16,6 +15,11 @@ if __name__ == "__main__":
     print("__________Generated strings___________")
     for i in range(5):
         string = grammar.generate_string()
-        print("Parsing:", finite_automaton.string_belongs_to_language(string))
-    print("=================================================")
-    print(finite_automaton.string_belongs_to_language("bccccdf"))
+        print("Validation:", finite_automaton.string_belongs_to_language(string))
+        print()
+
+    print("_________String validation___________")
+    test_string = "bdbbfd"
+    test_result = finite_automaton.string_belongs_to_language(test_string)
+    print("String:", test_string)
+    print("Validation:", test_result)
